268939bcb4abd5244541340ac64a9899db0239a8
Leslie <leslie.appl@gmail.com>
2021-01-02 12:48:40 +0800

    Finished XMLParserDelegate methods

--- IOSParseXMLTutorial210102/TableViewController.swift
+++ IOSParseXMLTutorial210102/TableViewController.swift
@@ -7,40 +7,53 @@
 
 import UIKit
 
+struct Book {
+    var bookTitle: String
+    var bookAuthor: String
+}
+
 class TableViewController: UITableViewController {
 
+    var books: [Book] = []
+    var elementName: String = String()
+    var bookTitle = String()
+    var bookAuthor = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        if let path = Bundle.main.url(forResource: "Books", withExtension: "xml") {
+            
+            if let parser = XMLParser(contentsOf: path) {
+                parser.delegate = self
+                parser.parse()
+            }
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return books.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let book = books[indexPath.row]
+         
+        cell.textLabel?.text = book.bookTitle
+        cell.detailTextLabel?.text = book.bookAuthor
+        
         return cell
     }
-    */
-
+ 
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -86,4 +99,50 @@ class TableViewController: UITableViewController {
     }
     */
 
+}
+
+extension TableViewController: XMLParserDelegate {
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        print("--Start parsing..")
+        if elementName == "book" {
+            bookTitle = String()
+            bookAuthor = String()
+        }
+        print("bookTitle: \(bookTitle)")
+        print("bookAuthor: \(bookAuthor)")
+        print("elementName: \(self.elementName)")
+        print("books: \(books)")
+        self.elementName = elementName
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        print("--End parsing..")
+        if elementName == "book" {
+            let book = Book(bookTitle: bookTitle, bookAuthor: bookAuthor)
+            books.append(book)
+        }
+        print("bookTitle: \(bookTitle)")
+        print("bookAuthor: \(bookAuthor)")
+        print("elementName: \(self.elementName)")
+        print("books: \(books)")
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        print("--Parsing char..")
+        let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        
+        if (!data.isEmpty) {
+            if self.elementName == "title" {
+                bookTitle += data
+            }
+            else if self.elementName == "author" {
+                bookAuthor += data
+            }
+        }
+        print("bookTitle: \(bookTitle)")
+        print("bookAuthor: \(bookAuthor)")
+        print("elementName: \(self.elementName)")
+        print("books: \(books)")
+    }
 }
