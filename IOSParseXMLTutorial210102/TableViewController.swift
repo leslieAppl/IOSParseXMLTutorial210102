6eb55907f9e6608af5ba2eecefceec0935935069
Leslie <leslie.appl@gmail.com>
2021-01-02 13:11:36 +0800

    Access And Read App’s Info.plist variables with Codable Struct

--- IOSParseXMLTutorial210102/TableViewController.swift
+++ IOSParseXMLTutorial210102/TableViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+struct Info: Codable {
+    
+    var CFBundleExecutable: String
+}
+
 struct Book {
     var bookTitle: String
     var bookAuthor: String
@@ -22,6 +27,18 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Access And Read App’s Info.plist variables with Codable Struct.
+        if  let path        = Bundle.main.path(forResource: "Info", ofType: "plist"),    //Info.plist file Name
+            let xml         = FileManager.default.contents(atPath: path),
+            let preferences = try? PropertyListDecoder().decode(Info.self, from: xml)    //Info Struct Name
+        {
+            print(preferences)
+        }
+        else {
+            return
+        }
+
+        // Access And Read XML file variables with XMLParserDelegate
         if let path = Bundle.main.url(forResource: "Books", withExtension: "xml") {
             
             if let parser = XMLParser(contentsOf: path) {
@@ -29,6 +46,7 @@ class TableViewController: UITableViewController {
                 parser.parse()
             }
         }
+        
     }
 
     // MARK: - Table view data source
@@ -109,10 +127,10 @@ extension TableViewController: XMLParserDelegate {
             bookTitle = String()
             bookAuthor = String()
         }
-        print("bookTitle: \(bookTitle)")
-        print("bookAuthor: \(bookAuthor)")
-        print("elementName: \(self.elementName)")
-        print("books: \(books)")
+//        print("bookTitle: \(bookTitle)")
+//        print("bookAuthor: \(bookAuthor)")
+//        print("elementName: \(self.elementName)")
+//        print("books: \(books)")
         self.elementName = elementName
     }
     
@@ -122,10 +140,10 @@ extension TableViewController: XMLParserDelegate {
             let book = Book(bookTitle: bookTitle, bookAuthor: bookAuthor)
             books.append(book)
         }
-        print("bookTitle: \(bookTitle)")
-        print("bookAuthor: \(bookAuthor)")
-        print("elementName: \(self.elementName)")
-        print("books: \(books)")
+//        print("bookTitle: \(bookTitle)")
+//        print("bookAuthor: \(bookAuthor)")
+//        print("elementName: \(self.elementName)")
+//        print("books: \(books)")
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
@@ -140,9 +158,9 @@ extension TableViewController: XMLParserDelegate {
                 bookAuthor += data
             }
         }
-        print("bookTitle: \(bookTitle)")
-        print("bookAuthor: \(bookAuthor)")
-        print("elementName: \(self.elementName)")
-        print("books: \(books)")
+//        print("bookTitle: \(bookTitle)")
+//        print("bookAuthor: \(bookAuthor)")
+//        print("elementName: \(self.elementName)")
+//        print("books: \(books)")
     }
 }
